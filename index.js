c7ff68dbf6300b543cb1679029d8ab0abbde6e24
gitAdalberto <adalbertogit@gmail.com>
2025-08-31 23:23:42 -0600

    apply removido

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(express.static.apply(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors({
     origin: "*",
