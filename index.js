5aa110be718a18346f0e76f3ddb7fcc9a1081c45
gitAdalberto <adalbertogit@gmail.com>
2025-08-31 23:20:35 -0600

    index.html estatico añadido

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 dotenv.config();
 
 import { pool } from './db.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
+
+app.use(express.static.apply(path.join(__dirname, "public")));
 
 app.use(cors({
     origin: "*",
